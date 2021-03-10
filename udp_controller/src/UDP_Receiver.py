b5b46234c8ea8d14593b49e4d0e03e76e873f606
ltiseni94 <luca13tiseni@gmail.com>
2021-03-10 11:19:26 +0100

    rtabmap working with wheel odometry (w/o sensor fusion) and lidar and rgbd camera. rviz configuration with robot urdf added

--- udp_controller/src/UDP_Receiver.py
+++ udp_controller/src/UDP_Receiver.py
@@ -5,47 +5,82 @@
 
 import rospy
 import socket
+import math
 import numpy as np
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import TwistWithCovarianceStamped
+import tf2_ros
+import tf
+import tf_conversions
+from geometry_msgs.msg import Twist, TransformStamped
+from geometry_msgs.msg import TwistWithCovarianceStamped, PoseWithCovariance, TwistWithCovariance
+from nav_msgs.msg import Odometry
 from kinematics_matrix import inv_jacobian, encoder_constant
 
 ######### DEFINE "GLOBAL" VARIABLES AND PARAMETERS #########
 
-#init encoder raw count value
 wh_speeds_enc = np.zeros(2, np.float32)
-#encoder input mask
 input_mask = encoder_constant * np.array([-1.0, 1.0], np.float32)
-# relative velocity and angular speed
 v_rel = np.array([0.0, 0.0], np.float32)   # [m/s], [m/s], [1/s]
-# odometry values
-# odom = np.array([0.0, 0.0, 0.0], np.float32)    # [m], [m], [rad]
-# old_odom = np.array([0.0, 0.0, 0.0], np.float32)    # [m], [m], [rad]
-# node rate and time step
 RATE = 100.0    # [Hz]
 dt = 1.0/RATE   # [s]
-# reset boolean: if 0 update current odometry, if 1 reset x, y, teta to 0
-# RESET_FLAG = False
+pub_rate = 20.0
+N = (int)(RATE/pub_rate)
 
-######### CUSTOM FUNCTIONS #########
+######### CUSTOM CLASSES #########
 
-# broadcaster of the odometry ad tf message
-# def handle_robot_pose(_odom):
-#    br = tf.TransformBroadcaster()  # create a tf
-#    br.sendTransform([_odom[0], _odom[1], 0.0], # load position
-#                     tf.transformations.quaternion_from_euler(0, 0, _odom[2]),  # load quaternion
-#                     rospy.Time.now(),  # send corrent time
-#                     "base_link",    # to
-#                     "odom")         # from
-
-#def reset_robot_pose(req):
-#    if req.reset == 1:
-#        global odom, old_odom
-#       # save the odometry before the reset
-#        old_odom = odom
-#        # reset odometry
-#        odom = np.array([0.0, 0.0, 0.0], np.float32)  # [m], [m], [rad]
-#        return reset_odomResponse(True)
+class RobotOdom():
+    def __init__(self, rate=RATE):
+        self.x=0
+        self.y=0
+        self.th=0
+        self.dt=1.0/rate
+        self.publisher = rospy.Publisher('odom_wheels', Odometry, queue_size=10)
+        self.tfbroadcaster = tf.TransformBroadcaster() # create a tf
+        self.twist_local = TwistWithCovariance()
+        self.twist_local.covariance[0]=1e-6
+        self.twist_local.covariance[7]=1e-6
+        self.twist_local.covariance[14]=1e-6
+        self.twist_local.covariance[21]=1e-6
+        self.twist_local.covariance[28]=1e-6
+        self.twist_local.covariance[35]=1e-4
+        self.odom_msg = Odometry()
+        
+    def update_twist(self, speed_array):
+        self.twist_local.twist.linear.x=speed_array[0]
+        self.twist_local.twist.angular.z=speed_array[1]
+        
+    def update_odom(self):
+        dx = self.twist_local.twist.linear.x*(math.cos(self.th))*self.dt
+        dy = self.twist_local.twist.linear.x*(math.sin(self.th))*self.dt
+        dth = self.twist_local.twist.angular.z*self.dt
+        self.x += dx
+        self.y += dy
+        self.th += dth
+        print(self.th)
+        
+    def publish_odom_msg(self):
+        now=rospy.Time.now()
+        self.odom_msg.header.stamp=now
+        self.odom_msg.header.frame_id="odom"
+        self.odom_msg.child_frame_id="base_link"
+        self.odom_msg.pose.pose.position.x=self.x
+        self.odom_msg.pose.pose.position.y=self.y
+        q=tf_conversions.transformations.quaternion_from_euler(0,0,self.th)
+        self.odom_msg.pose.pose.orientation.x=q[0]
+        self.odom_msg.pose.pose.orientation.y=q[1]
+        self.odom_msg.pose.pose.orientation.z=q[2]
+        self.odom_msg.pose.pose.orientation.w=q[3]
+        self.odom_msg.pose.covariance[0]=1e-4
+        self.odom_msg.pose.covariance[7]=1e-4
+        self.odom_msg.pose.covariance[35]=1e-3
+        self.odom_msg.twist=self.twist_local
+        
+        self.publisher.publish(self.odom_msg)
+        
+        self.tfbroadcaster.sendTransform([self.x, self.y, 0.0], # load position
+                q,                  # load quaternion
+                now,   # send current time
+                "base_link",        # to
+                "odom")             # from
 
 ######### SET LOCAL SOCKET IP ADDRESS AND UDP PORT #########
 personal_IP = "192.168.0.100"
@@ -55,66 +90,26 @@ sock.bind((personal_IP, personal_port))
 
 # ros init
 rospy.init_node('udp_receiver', anonymous=True)
-pub1 = rospy.Publisher('odom_wheels', Twist, queue_size=10)
-pub2 = rospy.Publisher('twist_n_covariance_wheels', TwistWithCovarianceStamped, queue_size=10)
-twist=Twist()
-tw_cov = TwistWithCovarianceStamped()
-# ros set rate
 r = rospy.Rate(RATE)
 
-print_counter = 0
-print_counter2 = 0
-
-# create the service
-# ser = rospy.Service('reset_msg', reset_odom, reset_robot_pose)
+robot_odom = RobotOdom()
+i=0
 
 while not rospy.is_shutdown():
-    # RECEIVE WHEEL VELOCITY CODE
-    # Receive incoming packet
-    data, _ = sock.recvfrom(10)   #receive 18 bytes
-    # interpret first 16 bytes as wheel velocities
+    data, _ = sock.recvfrom(10)
+    # interpret first 8 bytes as wheel velocities
     wh_speeds_enc = np.frombuffer(data, dtype=np.float32, count=2)*input_mask
     # interpret last 2 bytes as robot state
     enable_input = np.frombuffer(data, dtype=np.uint8, count=2, offset=8)
-    '''
-        # Print values on terminal
-        print_counter += 1
-        if print_counter > 100:
-            print(wh_speeds_enc * 30.0 / np.pi)
-            print(enable_input)
-            print_counter = 0
-    '''
-    #   CALCULATE ODOMETRY
     # calculate linear and angular relative speed of the robot
     v_rel = np.matmul(inv_jacobian, wh_speeds_enc)
-    twist.linear.x=v_rel[0]
-    twist.angular.z=v_rel[1]   
-    pub1.publish(twist)
+    robot_odom.update_twist(v_rel)
+    robot_odom.update_odom()
     
-    ### create twist message with stamp and covariance for robot loclization ###
-    tw_cov.header.stamp = rospy.Time.now()
-    tw_cov.header.frame_id = "odom"
-    # tw_cov.header.child_frame_id = "base_link"
+    if i == N:
+        robot_odom.publish_odom_msg()
+        i = 0
+    i+=1
     
-    tw_cov.twist.twist.linear.x=v_rel[0]
-    tw_cov.twist.twist.angular.z=v_rel[1] 
-    tw_cov.twist.covariance[0] = 1e-6
-    tw_cov.twist.covariance[35] = 1e-6
-    
-    pub2.publish(tw_cov)
-    
-    # update odom_positions
-    # odom += v_rel * dt
-    
-    #print_counter2 += 1
-    #if print_counter2 > 100:
-    #    print("vel = ", v_rel)
-    #    print("odom = ", odom * np.array([1.0, 180.0/np.pi]))
-    #   print_counter2 = 0
-    
-    # publish tf message
-    # handle_robot_pose(odom)
-    # graph()
-    # ROS SLEEP
     r.sleep()
 
